67a8ab765a878f2b2b8c10309f1ebde565ada698
Jennifer Ponder <jponder2000@yahoo.com>
2018-03-06 14:11:40 -0500

    wipupdate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//Create a game with four crystal crystals and a random result
+//Create a game with four crystal crystals and a random number to start
 //Each crystal will have a random number
 //The number will be randomly generated every time the game is won or lost to those 4 crystals
 //When a crystal is chosen by a click the number is adding to the previous until it equals to the total score 
@@ -10,34 +10,62 @@
 var randomNumber
 var win
 var lose
-
+var totalScore
 
 //FUNCTIONS
 //===================================================================
-randomNumberBetween19and120 = Math.floor(Math.random() * 19) +120;
 
-$("result").html('Random Result: ' + randomNumberBetween19and120);
+randomNumberBetween19and120 = Math.floor(Math.random() * (119 - 20) + 19);
+// console.log('randomNumber' =  + randNum);
+// var random = Math.floor(Math.random() * (12 - 1) + 1);
+crystal1 = Math.floor(Math.random() * (12 - 1) + 1);
+crystal2 = Math.floor(Math.random() * (12 - 1) + 1);
+crystal3 = Math.floor(Math.random() * (12 - 1) + 1);
+crystal4  = Math.floor(Math.random() * (12 - 1) + 1);
+totalScore = 0;
+
+
+$("#randomGiven").html(randomNumberBetween19and120);
 
 for(var i = 0; i < 4; i++){
 	console.log("Hello World")
+
+function gameStart(){
+	randomNumberBetween19and120 = Math.floor(Math.random() * (119 - 20) + 19);
+	// console.log('randomNumber' =  + randNum);
+	crystal1 = Math.floor(Math.random() * (12 - 1) + 1);
+	crystal2 = Math.floor(Math.random() * (12 - 1) + 1);
+	crystal3 = Math.floor(Math.random() * (12 - 1) + 1);
+	crystal4  = Math.floor(Math.random() * (12 - 1) + 1);
+totalScore = 0;
+
 }
 
 
-var random = Math.floor(Math.random() *11) + 1;
+$("#randomGiven").html(randomNumberBetween19and120);
+$('.totalScore').html(totalScore);
+gameStart();
+
+
+
+//Gives the crystal a random value
+$('.crystal1').attr("value", crystal1);
+var test = $('.crystal').attr("value");
+//Test it
+console.log('.crystal 1   '+test);
+
+
+
+
 console.log(random);
 
-var crystal = $("<div>");
-crystal.attr("class", 'crystal');
-"class": 'crystal',
-"data-random": random
 
-});
-
-
-$("crystals").append(crystal);
+if(randonNumber === totalScore){
+	alert('win');
+	win++;
 }
 
-
-$("crystal").on('click', function(){
-	console.log($(this));
+else if (totalScore > randomNumber){
+	alert('lose');
+	loss++;
 }
