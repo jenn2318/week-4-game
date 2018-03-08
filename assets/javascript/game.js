4d6308fa2e76b893210ba88aa5e3c9c92c366280
Jennifer Ponder <jponder2000@yahoo.com>
2018-03-07 19:44:03 -0500

    wipupdated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,67 @@
-//Create a game with four crystal crystals and a random number to start
-//Each crystal will have a random number
+//Create a game that starts with a random number between 19 and 120
+//Each crystal will have a random number between 1 and 12
 //The number will be randomly generated every time the game is won or lost to those 4 crystals
 //When a crystal is chosen by a click the number is adding to the previous until it equals to the total score 
 //If it does not equal to the score the game starts over
 //If it equals, then the win counter will increment
-
 //VARIABLES
+$(document).ready(function) {
 //===================================================================
-var randomNumber
-var win
-var lose
-var totalScore
-
+var randomNumber = 0
+var win = 0
+var lose = 0
+var totalScore = 0
+var counter = 0
+var crystal1 = 0
+var crystal2 = 0
+var crystal3 = 0
+var crystal4 = 0
+var score = 0
 //FUNCTIONS
 //===================================================================
-
-randomNumberBetween19and120 = Math.floor(Math.random() * (119 - 20) + 19);
-// console.log('randomNumber' =  + randNum);
-// var random = Math.floor(Math.random() * (12 - 1) + 1);
-crystal1 = Math.floor(Math.random() * (12 - 1) + 1);
-crystal2 = Math.floor(Math.random() * (12 - 1) + 1);
-crystal3 = Math.floor(Math.random() * (12 - 1) + 1);
-crystal4  = Math.floor(Math.random() * (12 - 1) + 1);
-totalScore = 0;
-
-
-$("#randomGiven").html(randomNumberBetween19and120);
-
-for(var i = 0; i < 4; i++){
-	console.log("Hello World")
-
-function gameStart(){
+//Calculates a random number for the game
+function getRandomNumber(){
 	randomNumberBetween19and120 = Math.floor(Math.random() * (119 - 20) + 19);
-	// console.log('randomNumber' =  + randNum);
+	$("#randomNumber").html(randomNumberBetween19and120);
+}
+//Calculates random numbers for the crystals
+function getCrystalRandomNumber(){
 	crystal1 = Math.floor(Math.random() * (12 - 1) + 1);
 	crystal2 = Math.floor(Math.random() * (12 - 1) + 1);
 	crystal3 = Math.floor(Math.random() * (12 - 1) + 1);
 	crystal4  = Math.floor(Math.random() * (12 - 1) + 1);
-totalScore = 0;
+$("#crystalButtons").html(randomNumberBetween1and12)
+};
 
-}
+//Adds score to crystals
+//=================================================================
 
-
-$("#randomGiven").html(randomNumberBetween19and120);
-$('.totalScore').html(totalScore);
-gameStart();
-
-
-
-//Gives the crystal a random value
-$('.crystal1').attr("value", crystal1);
-var test = $('.crystal').attr("value");
-//Test it
-console.log('.crystal 1   '+test);
-
-
-
-
-console.log(random);
-
-
+$(".crystal1").on("click", function(){
+	totalScore = totalScore + crystal1;
+	$('#total-score').html(totalScore);
+calculateTotal();
+});
+//FUNCTION decides win or loss
+//==================================================================
+function decideWinLoss() {
 if(randonNumber === totalScore){
-	alert('win');
+	$('#win').text(wins);
 	win++;
+	alert('win');
+	reset();
+}
+else if (totalScore > randomNumber){
+	$('#loss').text(losses);
+	loss++;
+    alert('lose');
+    reset();
+  }
+}
+//FUNCTION resets the game
+ function reset() {
+   totalScore = 0;
+   randomNumber = 0;
+   $('#score').html(totalScore);  
 }
 
-else if (totalScore > randomNumber){
-	alert('lose');
-	loss++;
-}
+});
